c51c094b4ce4bcfab9d49286f8413f0aa55c8b75
Joshua Packard <thegrandpackard@gmail.com>
2023-10-15 18:52:24 -0700

    Implement music player

---
+++ cmd/musicplayer/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/thegrandpackard/PackardQuest/musicplayer"
+)
+
+var (
+	player = musicplayer.NewMusicPlayer(
+		[]string{
+			"01 - Prologue.mp3",
+			"02 - Harry's Wondrous World.mp3",
+			"03 - The Arrival Of Baby Harry.mp3",
+			"04 - Visit To The Zoo And Letters From Hogwarts.mp3",
+			"05 - Diagon Alley And The Gringotts Vault.mp3",
+			"06 - Platform Nine-and-three-quarters And The Journey To Hogwarts.mp3",
+			"07 - Entry Into The Great Hall And The Banquet.mp3",
+			"08 - Mr. Longbottom Flies.mp3",
+			"09 - Hogwarts Forever! And The Moving Stairs.mp3",
+			"10 - The Norwegian Ridgeback And A Change Of Season.mp3",
+			"11 - The Quidditch Match.mp3",
+			"12 - ChristmasAtHogwarts.mp3",
+			"13 - TheInvisiblityCloackAndTheLibraryScene.mp3",
+			"14 - FluffysHarp.mp3",
+			"15 - InTheDevilsSnareAndTheFlyingKeys.mp3",
+			"16 - TheChessGame.mp3",
+			"17 - TheFaceOfVoldemort.mp3",
+			"18 - LeavingHogwarts.mp3",
+			"19 - HedwigsTheme.mp3",
+		},
+	)
+)
+
+func main() {
+	log.Print("Music Player")
+
+	go processStdIn()
+
+	// Capture Ctrl-c to shut down server
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+}
+
+func processStdIn() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	log.Println("Controls: [p]lay, [s]top, [n]ext, [l]ist (press Ctrl+C to end)")
+
+	// Read input line by line
+	for scanner.Scan() {
+		text := scanner.Text() // Get the current line of text
+		if text == "" {
+			break // Exit loop if an empty line is entered
+		}
+
+		switch text {
+		case "p", "play":
+			if err := player.Play(); err != nil {
+				log.Println("Error playing:", err.Error())
+				continue
+			}
+			log.Println("Playing:", player.GetCurrentSongName())
+		case "s", "stop":
+			if err := player.Stop(); err != nil {
+				log.Println("Error stopping:", err.Error())
+				continue
+			}
+			log.Println("Stopped")
+		case "n", "next":
+			if err := player.Next(); err != nil {
+				log.Println("Error playing next:", err.Error())
+				continue
+			}
+			log.Println("Playing next song:", player.GetCurrentSongName())
+		case "l", "list":
+			log.Println("Playlist:", strings.Join(player.GetSongs(), ", "))
+		default:
+			log.Println("Unknown command:", text)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println("Error:", err)
+	}
+}
---
+++ musicplayer/musicplayer.go
@@ -0,0 +1,150 @@
+package musicplayer
+
+import (
+	"errors"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gopxl/beep"
+	"github.com/gopxl/beep/mp3"
+	"github.com/gopxl/beep/speaker"
+	"github.com/gopxl/beep/vorbis"
+)
+
+type MusicPlayer interface {
+	GetCurrentSongName() string
+	GetSongs() []string
+
+	Play() error
+	Stop() error
+	Next() error
+}
+
+type musicPlayer struct {
+	currentSong *int
+	songs       []string
+
+	streamer beep.StreamSeekCloser
+	ctrl     *beep.Ctrl
+	doneChan chan bool
+}
+
+func NewMusicPlayer(songs []string) MusicPlayer {
+	p := &musicPlayer{
+		songs:    songs,
+		doneChan: make(chan bool),
+	}
+
+	// handle song done to play next
+	go p.doneHandler()
+
+	return p
+}
+
+func (p *musicPlayer) doneHandler() {
+	for {
+		<-p.doneChan
+		if err := p.Next(); err != nil {
+			log.Println("Error playing next:", err.Error())
+			continue
+		}
+		log.Println("Playing next song: ", p.GetCurrentSongName())
+	}
+}
+
+func (p *musicPlayer) GetCurrentSongName() string {
+	if p.currentSong == nil {
+		return "No current song selected"
+	}
+	return p.songs[*p.currentSong]
+}
+
+func (p *musicPlayer) GetSongs() []string {
+	return p.songs
+}
+
+func (p *musicPlayer) Play() error {
+	if p.currentSong == nil {
+		if err := p.getNextSong(); err != nil {
+			return err
+		}
+	}
+
+	return p.play()
+}
+
+func (p *musicPlayer) Stop() error {
+	return p.stop()
+}
+
+func (p *musicPlayer) Next() error {
+	if err := p.getNextSong(); err != nil {
+		return err
+	}
+
+	return p.play()
+}
+
+func (p *musicPlayer) getNextSong() error {
+	if p.currentSong == nil {
+		if len(p.songs) == 0 {
+			return errors.New("no songs in player")
+		}
+
+		idx := 0
+		p.currentSong = &idx
+	} else {
+		if *p.currentSong+1 >= len(p.songs) {
+			idx := 0
+			p.currentSong = &idx
+		} else {
+			idx := *p.currentSong + 1
+			p.currentSong = &idx
+		}
+	}
+
+	return nil
+}
+
+func (p *musicPlayer) play() error {
+	fileName := p.songs[*p.currentSong]
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var format beep.Format
+
+	if strings.HasSuffix(fileName, ".mp3") {
+		p.streamer, format, err = mp3.Decode(f)
+	} else if strings.HasSuffix(fileName, ".ogg") {
+		p.streamer, format, err = vorbis.Decode(f)
+	} else {
+		return errors.New("unsupported format for file:" + fileName)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	p.ctrl = &beep.Ctrl{Streamer: beep.Loop(1, p.streamer), Paused: false}
+
+	speaker.Play(beep.Seq(p.ctrl, beep.Callback(func() {
+		p.doneChan <- true
+	})))
+
+	return nil
+}
+
+func (p *musicPlayer) stop() error {
+	if p.currentSong != nil {
+		speaker.Lock()
+		p.ctrl.Paused = true
+		speaker.Unlock()
+	}
+
+	return nil
+}
